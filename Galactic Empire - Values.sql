3d2cef3b8b5c221fe8193fe46101ba47e78ac890
Lauren Perkins <97481606+laurenperkins11364@users.noreply.github.com>
2023-10-17 20:06:33 -0400

    Create Galactic Empire - Values.sql

---
+++ Galactic Empire - Values.sql
@@ -0,0 +1,138 @@
+INSERT INTO SYSTEMS VALUES ('Coruscant', '0,0,0', '3', 'Controlled');
+INSERT INTO SYSTEMS VALUES ('Tatooine', '-10,-11,-12', '3', 'Neutral');
+INSERT INTO SYSTEMS VALUES ('Alderaan', '15,-10,5', '1', 'Depopulated');
+INSERT INTO SYSTEMS VALUES ('Hoth', '30,-40,60', '3', 'Unpopulated');
+INSERT INTO SYSTEMS VALUES ('Endor', '80,-60,90', '1', 'Controlled');
+INSERT INTO SYSTEMS VALUES ('Bespin', '-20,50,30', '2', 'Neutral');
+INSERT INTO SYSTEMS VALUES ('Naboo', '-80,-30,60', '3', 'Controlled');
+INSERT INTO SYSTEMS VALUES ('Kamino', '-100,-50,10', '1', 'Controlled');
+INSERT INTO SYSTEMS VALUES ('Geonosis', '-200,-100,20', '3', 'Neutral');
+INSERT INTO SYSTEMS VALUES ('Yavin', '50,-80,30', '1', 'Hostile');
+INSERT INTO SYSTEMS VALUES ('Ryloth', '-35,-325,543', '2', 'Hostile');
+INSERT INTO SYSTEMS VALUES ('Utapau', '385,358,90', '6', 'Controlled');
+INSERT INTO SYSTEMS VALUES ('Crait', '85,-79,21', '3', 'Controlled');
+INSERT INTO SYSTEMS VALUES ('Inyusu Tor', '37,-53,72', '4', 'Hostile');
+INSERT INTO SYSTEMS VALUES ('Scarif', '34,483,-123', '9', 'Controlled');
+INSERT INTO SYSTEMS VALUES ('Chandrila', '392,753,-395', '0', 'Controlled');
+INSERT INTO SYSTEMS VALUES ('Dantooine', '-285,43,-40', '2', 'Neutral');
+INSERT INTO SYSTEMS VALUES ('Kashyyyk', '274,382,192', '1', 'Hostile');
+INSERT INTO SYSTEMS VALUES ('Rishi', '-28,-374,-71', '13', 'Controlled');
+INSERT INTO SYSTEMS VALUES ('Arkanis', '738,275,-246', '2', 'Controlled');
+INSERT INTO SYSTEMS VALUES ('Ord Mantell', '283,48,-38', '8','Neutral');
+INSERT INTO SYSTEMS VALUES ('Lothal', '673,37,-436', '5', 'Hostile');
+INSERT INTO SYSTEMS VALUES ('Shili', '-37,84,-37', '3', 'Neutral');
+
+
+INSERT INTO MOFFS VALUES ('Willhuff', 'Tarkin', '1977-05-25', 'Grand Moff', 'Coruscant', '123456789');
+INSERT INTO MOFFS VALUES ('Conan', 'Motti',	'1981-11-21', 'General', 'Tatooine', '539217864');
+INSERT INTO MOFFS VALUES ('Cassio', 'Tagge', '1980-03-24', 'Grand Moff', 'Alderaan', '761289430');
+INSERT INTO MOFFS VALUES ('Delian', 'Mors', '1981-02-13', 'General', 'Alderaan', '942837615');
+INSERT INTO MOFFS VALUES ('Treuten', 'Teradoc', '1980-07-31', 'Admiral', 'Hoth', '184736529');
+INSERT INTO MOFFS VALUES ('Kendal', 'Ozzel', '2000-01-01', 'Admiral', 'Dantooine', '627598341');
+INSERT INTO MOFFS VALUES ('Tiann', 'Jerjerrod', '1983-05-25', 'General', 'Endor', '749275395');
+INSERT INTO MOFFS VALUES ('Sarn', 'Shild', '1900-07-25', 'Admiral', 'Bespin', '315487926');
+INSERT INTO MOFFS VALUES ('Quarsh', 'Panaka', '1979-08-15', 'General', 'Naboo', '890654123');
+INSERT INTO MOFFS VALUES ('Droid', 'AD-3', '2002-06-04', 'General', 'Kamino', '479302658');
+INSERT INTO MOFFS VALUES ('Sunfac', 'Page', '2009-07-08', 'Admiral', 'Geonosis', '256839047');
+INSERT INTO MOFFS VALUES ('Sutton', 'Tion', '2021-03-04', 'Admiral', 'Yavin', '856839094');
+
+
+INSERT INTO CAMPAIGNS VALUES ('Siege of Ryloth', '184736529', '740219856', 'Ongoing', '1992-07-18', '250012');
+INSERT INTO CAMPAIGNS VALUES ('Battle of Yavin 4', '123456789', '913257486', 'Defeat', '1987-02-10', '2350000');
+INSERT INTO CAMPAIGNS VALUES ('Battle of Hoth', '539217864', '275109863', 'Victory', '1978-11-27', '250');
+INSERT INTO CAMPAIGNS VALUES ('Battle of Graffus', '761289430', '462983175', 'Ongoing', '2001-05-03', '45064');
+INSERT INTO CAMPAIGNS VALUES ('Defense of Inyusu Tor', '627598341', '817654329', 'Defeat', '1964-09-22', '553842');
+INSERT INTO CAMPAIGNS VALUES ('Peacekeeping in Crait', '539217864', '639821574', 'Victory', '1980-12-31', '17');
+INSERT INTO CAMPAIGNS VALUES ('Defense of Scarif', '942837615', '572390184', 'Ongoing', '1999-08-14', '24528');
+INSERT INTO CAMPAIGNS VALUES ('Operation: Knightfall', '749275395', '198765432', 'Victory', '1975-03-07', '5378');
+INSERT INTO CAMPAIGNS VALUES ('Battle of Endor', '315487926', '543297681', 'Defeat', '2004-01-15', '3702759');
+INSERT INTO CAMPAIGNS VALUES ('Defense of Utapau', '890654123', '326719854', 'Ongoing', '1990-06-09', '738932');
+
+
+INSERT INTO GENERALS VALUES ('Clark', 'Kent', '164521659', '2015-08-11', '326719854', '5', '1995-11-02');
+INSERT INTO GENERALS VALUES ('Maximilian', 'Veers', '935012867', '2006-01-12', '462983175', '3', '1988-08-17');
+INSERT INTO GENERALS VALUES ('Optimus', 'Prime', '479315628', '2023-04-25', '572390184', '2', '2001-05-29');
+INSERT INTO GENERALS VALUES ('Cassio', 'Tagge', '761902384', '1993-09-05', '326719854', '1', '1979-12-08');
+INSERT INTO GENERALS VALUES ('Kassius', 'Konstantine', '862540917', '2005-02-23', '572390184', '0', '1998-04-03');
+INSERT INTO GENERALS VALUES ('Rae', 'Sloane', '143276589', '2020-11-22', '740219856', '2', '2002-10-04');
+INSERT INTO GENERALS VALUES ('Thawn', 'Ozzel', '528746391', '2022-07-23', '740219856', '1', '2003-01-31');
+INSERT INTO GENERALS VALUES ('Gallius', 'Rax', '695830142', '2013-03-12', '462983175', '1', '1992-07-06');
+INSERT INTO GENERALS VALUES ('Brendol', 'Hux', '217594683', '2013-10-15', '572390184', '0', '2003-03-25');
+INSERT INTO GENERALS VALUES ('Kaleth', 'Krennix', '183749625', '2010-08-08', '740219856', '4', '1978-06-14');
+
+
+INSERT INTO ADMIRALS VALUES ('Gial', 'Ackbar', '427890156', '1981-07-13', '740219856', '2', '1945-09-07');
+INSERT INTO ADMIRALS VALUES ('Antonio', 'Motti', '815067239', '1979-11-09', '462983175', '0', '1950-08-20');
+INSERT INTO ADMIRALS VALUES ('Kendel', 'Ozzel', '692481375', '1977-01-03', '913257486', '0', '1947-03-12');
+INSERT INTO ADMIRALS VALUES ('Firmus', 'Piett', '239705816', '1980-09-25', '326719854', '2', '1940-05-06');
+INSERT INTO ADMIRALS VALUES ('Ozzel', 'Maximilian', '587319462', '1980-02-17', '198765432', '1', '1935-12-18');
+INSERT INTO ADMIRALS VALUES ('Raddus', 'Sloane', '946127538', '1981-08-18', '572390184', '4', '1951-07-15');
+INSERT INTO ADMIRALS VALUES ('Versio', 'Iden', '351902684', '1979-11-20', '543297681', '2', '1947-11-09');
+INSERT INTO ADMIRALS VALUES ('Zara', 'Krefayn', '381928372', '1981-06-08', '817654329', '4', '1950-04-23');
+INSERT INTO ADMIRALS VALUES ('Firmus', 'Piett', '987123456', '1980-05-21', '543297681', '3', '1955-02-27');
+INSERT INTO ADMIRALS VALUES ('Garret', 'Fyldor', '905706808', '1978-07-23', '639821574', '2', '1942-10-31');
+
+
+INSERT INTO BASE VALUES ('Echo Base', '748932516', 'Hoth', 'Abandoned', '30,-40,60');
+INSERT INTO BASE VALUES ('Death Star I', '236710945', 'Alderaan', 'Destroyed', '15,-10,5');
+INSERT INTO BASE VALUES ('Death Star II', '891563274', 'Endor', 'Active', '80,-60,90');
+INSERT INTO BASE VALUES ('Starkiller Base', '475319826', 'Scarif', 'Destroyed', '0,0,0');
+INSERT INTO BASE VALUES ('Cloning facility', '659278134', 'Kamino', 'Active', '-100,-50,10');
+INSERT INTO BASE VALUES ('Theed Palace', '213597468', 'Naboo', 'Active', '-80,-30,60');
+INSERT INTO BASE VALUES ('Jedi Temple', '987126354', 'Coruscant', 'Destroyed', '0,10,20');
+INSERT INTO BASE VALUES ('Imperial Academy', '519283764', 'Coruscant', 'Active', '-10,-11,-12');
+INSERT INTO BASE VALUES ('Cloud City', '327419856', 'Bespin', 'Active', '-20,50,30');
+INSERT INTO BASE VALUES ('Droid Factory', '946781253', 'Geonosis', 'Abandoned', '-200,-100,20');
+INSERT INTO BASE VALUES ('Shield Bunker 5', '464826592', 'Endor', 'Active', '-284, -138, 82');
+
+
+INSERT INTO LEGIONS VALUES ('501st Battalion', '183037535', '862540917', '659278134', '9216', 'Heavy Assault Troopers');
+INSERT INTO LEGIONS VALUES ('212th Attack Battalion', '375829573', '217594683', '659278134', '7212', 'Infrantry and Armor');
+INSERT INTO LEGIONS VALUES ('91st Reconnaissance Corps', '384938593', '761902384', '213597468', '3864', 'Reconnaissance and Surviellance');
+INSERT INTO LEGIONS VALUES ('327th Star Corps', '759335937', '479315628', '464826592', '7212', 'Infrantry and Armor');
+INSERT INTO LEGIONS VALUES ('41st Elite Corps', '760473057', '143276589', '659278134', '3864', 'Jungle and Urban Warfare');
+INSERT INTO LEGIONS VALUES ('332nd Galactic Corps', '820472047', '164521659', '464826592', '576', 'Clone Commandos');
+INSERT INTO LEGIONS VALUES ('1212th Reconnaissance Battalion', '203382753', '695830142', '327419856', '1212', 'Reconnaissance and Surviellance');
+INSERT INTO LEGIONS VALUES ('21st Nova Corps', '372946294', '183749625', '519283764', '2323', 'Infiltration and Sabotage');
+INSERT INTO LEGIONS VALUES ('104th Wolfpack Battalion', '369463927', '479315628', '519283764', '1598', 'Jungle and Urban Warfare');
+INSERT INTO LEGIONS VALUES ('181st Armor Division', '836593658', '183749625', '213597468', '2930', 'Vehicular Combat and Troop Transport');
+
+
+INSERT INTO FLEETS VALUES ('Imperial I Fleet', '159468531', '427890156', 'Coruscant', '280');
+INSERT INTO FLEETS VALUES ('Imperial II Fleet', '150089563', '815067239', 'Endor', '200');
+INSERT INTO FLEETS VALUES ('Death Squadron', '124875963', '692481375', 'Naboo', '60');
+INSERT INTO FLEETS VALUES ('Seventh Fleet', '124870023', '239705816', 'Yavin', '120');
+INSERT INTO FLEETS VALUES ('Tenth Fleet', '245789637', '587319462', 'Geonosis', '80');
+INSERT INTO FLEETS VALUES ('Twelfth Fleet', '100478965', '946127538', 'Scarif', '120');
+INSERT INTO FLEETS VALUES ('Phoenix Home Fleet', '120458795', '351902684', 'Ryloth', '400');
+INSERT INTO FLEETS VALUES ('Pelta-class Frigate Fleet', '58759650', '351902684', 'Kamino', '120');
+INSERT INTO FLEETS VALUES ('Separatist Navy', '87595315', '987123456', 'Utapau', '80');
+INSERT INTO FLEETS VALUES ('Corellian Defense Fleet', '957120684', '905706808', 'Crait', '60');
+INSERT INTO FLEETS VALUES ('Black Fleet', '162487462', '946127538', 'Inyusu Tor', '500');
+INSERT INTO FLEETS VALUES ('First Order Fleet', '157409546', '351902684', 'Chandrila', '300');
+INSERT INTO FLEETS VALUES ('Resistance Fleet', '125078945', '351902684', 'Dantooine', '250');
+INSERT INTO FLEETS VALUES ('Chriss Ascendancy Defense Fleet', '365012789', '987123456', 'Bespin', '100');
+INSERT INTO FLEETS VALUES ('Corporate Sector Authority Fleet', '354216987', '905706808', 'Shili', '150');
+
+
+INSERT INTO STORMTROOPERS VALUES ('Jan', 'Erso', '321987654', 'Shili', '1988-12-15', 'Private', '183037535');
+INSERT INTO STORMTROOPERS VALUES ('Cassian', 'Andor', '589036247', 'Naboo', '1977-09-01', 'Captain', '375829573');
+INSERT INTO STORMTROOPERS VALUES ('K-2SO', 'N/A', '467813295', 'Kashyyyk', '1994-03-24', 'Enforcer', '384938593');
+INSERT INTO STORMTROOPERS VALUES ('TK-421', 'SL-413', '932156487', 'Geonosis', '1965-07-11', 'Sergeant', '759335937');
+INSERT INTO STORMTROOPERS VALUES ('CT-7567', 'Rex', '874531290', 'Kamino', '2003-05-29', 'Captain', '183037535');
+INSERT INTO STORMTROOPERS VALUES ('CC-2224', 'Cody', '256478913', 'Kamino', '1979-11-08', 'Commander', '760473057');
+INSERT INTO STORMTROOPERS VALUES ('TK-146275', 'N/A', '609842571', 'Alderaan', '1992-01-21', 'Stormtrooper', '820472047');
+INSERT INTO STORMTROOPERS VALUES ('FN-2187', 'Finn', '735210896', 'Coruscant', '1986-08-04', 'Stormtrooper', '203382753');
+INSERT INTO STORMTROOPERS VALUES ('Unknown', 'Phasma', '178945623', 'Tatooine', '1972-02-14', 'Captain', '820472047');
+
+
+INSERT INTO PILOTS VALUES ('Kara', 'Sorenson', '485923710', 'Chandrila', '2000-01-01', 'Lieutenant', '159468531');
+INSERT INTO PILOTS VALUES ('Jaxon', 'Kallik', '763218459', 'Dantooine', '2010-05-07', 'Major', '150089563');
+INSERT INTO PILOTS VALUES ('Rian', 'Thorne', '902315678', 'Kashyyyk', '2005-07-09', 'Private', '124875963');
+INSERT INTO PILOTS VALUES ('Eira', 'Solari', '574836291', 'Rishi', '2015-11-29', 'Sergeant', '245789637');
+INSERT INTO PILOTS VALUES ('Devan', 'Naxos', '316598427', 'Arkanis', '2009-09-09', 'Captain', '957120684');
+INSERT INTO PILOTS VALUES ('Kael', 'Valtor', '840172936', 'Ord Mantell', '2000-10-10', 'Captain', '58759650');
+INSERT INTO PILOTS VALUES ('Lira', 'Kyrell', '627195384', 'Lothal', '1998-10-05', 'Lieutenant', '87595315');
+INSERT INTO PILOTS VALUES ('Taro', 'Vos', '139647582', 'Shili', '2000-02-10', 'Private', '365012789');
+INSERT INTO PILOTS VALUES ('Nia', 'Sunrider', '295730846', 'Alderaan', '1981-04-14', 'Private', '157409546');
+INSERT INTO PILOTS VALUES ('Gage', 'Rendar', '768429135', 'Bespin', '1978-08-14', 'Private', '100478965');
